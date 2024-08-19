3333bacf3bcef9fec8c9ffc796d8927b724d6ac9
harilal766 <harilalsunil2@gmail.com>
2024-08-19 21:22:21 +0530

    settings.py : static root - static-blockbuster

--- Movie_DB/settings.py
+++ Movie_DB/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static-blockbuster")
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static-blockbuster')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
